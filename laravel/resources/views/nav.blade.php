577e6c4c32d727d6c5245a353d574f9e43d67ebb
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-04-26 14:11:54 +0900

    投稿テーブル作成

--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -7,5 +7,32 @@
     <li class="nav-item">
       <a href="" class="nav-link">ユーザー登録</a>
     </li>
+
+    <li class="nav-item">
+      <a href="" class="nav-link">ログイン</a>
+    </li>
+
+    <li class="nav-item">
+      <a href="" class="nav-link"><i class="fas fa-pen mr-1"></i>投稿する</a>
+    </li>
+
+    {{-- Dropdown --}}
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-user-circle"></i>
+      </a>
+      <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+        <button class="dropdown-item" type="button" onclick="location.href=''">
+          マイページ
+        </button>
+        <div class="dropdown-divider"></div>
+        <button form="logout-button" class="dropdown-item" type="submit">
+          ログアウト
+        </button>
+      </div>
+    </li>
+    <form action="" method="post" id="logout-button">
+    </form>
+    {{-- Dropdown --}}
   </ul>
 </nav>
