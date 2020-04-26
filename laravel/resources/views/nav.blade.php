c958d9ccc3081f8b60a77a7759cde7ab4d53f5af
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-04-26 10:42:10 +0900

    トップページ作成

---
+++ laravel/resources/views/nav.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand navbar-dark blue-gradient">
+
+  <a href="/" class="navbar-bland"><i class="far fa-sticky-note mr-1"></i>memo</a>
+
+  <ul class="navbar-nav ml-auto">
+
+    <li class="nav-item">
+      <a href="" class="nav-link">ユーザー登録</a>
+    </li>
+  </ul>
+</nav>
